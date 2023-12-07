a3fcbe5cf9ca6af704a293bb55935b9875e99d6e
Muhammad Ansar <109860407+ansar062@users.noreply.github.com>
2023-12-07 17:42:09 +0500

    filter no working

---
+++ derma-web/data/dataprovider/locationsData.tsx
@@ -0,0 +1,35 @@
+
+
+const locatios = [
+    {
+        id: "lhr",
+        name: "Lahore",
+    },
+    {
+        id: "skp",
+        name: "Sheikhupura",
+    },
+    {
+        id: "sialkot",
+        name: "Sialkot",
+    },
+    {
+        id: "fsb",
+        name: "Faisalabad",
+    },
+    {
+        id: "gjrt",
+        name: "Gujrat",
+    },
+    {
+        id: "islamabad",
+        name: "Islamabad",
+    },
+    {
+        id: "pindi",
+        name: "Pindi",
+    },
+
+]
+
+export default locatios;
--- derma-web/src/app/components/doctors/page.tsx
+++ derma-web/src/app/components/doctors/page.tsx
@@ -1,7 +1,25 @@
 import DoctorsCard from "./DoctorsCard";
 import sampleDoctorData from "../sampleDoctorData";
+import React, { useState } from "react";
+import Button from "./Button";
+import locatios from "../../../../data/dataprovider/locationsData";
+import FilterDrawer from "./Filter/filter";
 
 const Doctors = () => {
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [filters, setFilters] = useState<string[]>([]);
+    const handleDrawerOpen = () => {
+        setIsDrawerOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setIsDrawerOpen(false);
+    };
+
+    const handleApplyFilters = () => {
+        // Apply filters logic here
+        setIsDrawerOpen(false);
+    };
     return (
         <div>
             <div className="relative">
@@ -13,6 +31,21 @@ const Doctors = () => {
                 <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-5xl font-bold leading-none text-center text-white">
                     Meet Our Doctors
                 </h1>
+            </div>
+            <div>
+                <button
+                    className="my-4 p-2 bg-blue-500 text-white"
+                    onClick={handleDrawerOpen}
+                >
+                    Open Filter Drawer
+                </button>
+                <FilterDrawer
+                    isOpen={isDrawerOpen}
+                    onClose={handleDrawerClose}
+                    onApply={handleApplyFilters}
+                    filters={filters}
+                    setFilters={setFilters}
+                />
             </div>
 
             {sampleDoctorData.doctors.map((id) => (
@@ -33,9 +66,9 @@ const Doctors = () => {
                     />
                 </div>
             ))}
-
         </div>
     );
 };
 
 export default Doctors;
+
